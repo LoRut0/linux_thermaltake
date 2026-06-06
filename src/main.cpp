745cc05d77b58b4eb45ce3fe358aa4ee401b9b9d
LoRuto <yaemkapustu37@gmail.com>
2026-06-07 02:53:56 +0300

    added systemd service

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     spdlog::set_level(spdlog::level::info);
 
-    std::string path = "config.yml";
+    std::string path = "/usr/local/etc/thermaltake-controller/config.yml";
     ThermaltakeController controller(Config::loadDevices(path).size(), path);
     controller.start();
     return 0;
